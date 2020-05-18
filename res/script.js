313cc24233e909326af10057918841e6f26d5c56
Andrey Oleynik <andre.oleynik@gmail.com>
2020-05-19 00:56:18 +0300

    Update script.js

--- res/script.js
+++ res/script.js
@@ -27,7 +27,20 @@ data = {
     ]
 };
 
-//render users list
+let itemList;
+let isActivItem = 0;
+let isActivButton = true;
+let newUserButton = document.querySelector('span[data-action=newUser]');
+let addUserButton = document.querySelector('span[data-action=addUser]');
+let cancelButton = document.querySelector('span[data-action=cancel]');
+let buttonList = document.querySelectorAll('span[data-action]');
+let screenList = document.querySelectorAll('main>section');
+let screenOne = document.querySelector('.account-list');
+let screenTwo = document.querySelector('.account-add');
+let items = document.querySelector('.items');
+let addNewAccountInput = document.querySelector('.account-add input');
+
+//render accounts list
 function LoadUserAccounts(data, id = 0) {
     let list = '';
     data.forEach((element, index) => {
@@ -38,9 +51,10 @@ function LoadUserAccounts(data, id = 0) {
                 </div>`;
     });
     if (list) {
-        document.querySelector('.items').innerHTML = list;
+        items.innerHTML = list;
+        itemList = document.querySelectorAll('.item');
     } else {
-        document.querySelector('.items').innerHTML = '<h4>No users</h4>';
+        items.innerHTML = '<h4>No accounts</h4>';
     }
     if (data.length != 0) {
         scroll();
@@ -70,53 +84,45 @@ document.addEventListener('keydown', function (event) {
     }
 });
 
-var index = 0;
-var index_adding = 0;
+
+
 
 function leftPress() {
     //screen 1
-    if (!document.querySelector('.account-list').classList.contains('hidden')) {
-        if (document.querySelector('span[data-action=newUser]').classList.contains('focus')) {
-            for (const iterator of document.querySelectorAll('.item')) {
-                if (iterator.dataset.id === index) {
+    if (!screenOne.classList.contains('hidden')) {
+        if (newUserButton.classList.contains('focus')) {
+            for (const iterator of itemList) {
+                if (iterator.dataset.id === isActivItem) {
                     iterator.classList.add('focus');
                 }
             }
-            document.querySelector('span[data-action=newUser]').classList.remove('focus');
+            newUserButton.classList.remove('focus');
         } else {
-            for (const iterator of document.querySelectorAll('.item')) {
+            for (const iterator of itemList) {
                 if (iterator.classList.contains('focus')) {
-                    if (iterator.previousElementSibling) {
+                    if (iterator.dataset.id > 0) {
                         data.accounts.splice(iterator.dataset.id, 1);
                         LoadUserAccounts(data.accounts, iterator.dataset.id - 1);
-                        return;
-                    } else if (iterator.nextElementSibling) {
-                        data.accounts.splice(iterator.dataset.id, 1);
-                        LoadUserAccounts(data.accounts);
                         return;
                     } else {
                         data.accounts.splice(iterator.dataset.id, 1);
                         LoadUserAccounts(data.accounts);
-                        document.querySelector('span[data-action=newUser]').classList.add('focus');
+                        if (data.accounts.length === 0) {
+                            newUserButton.classList.add('focus');
+                        }
                         return;
                     }
-
                 }
             }
         }
 
     }
     //screen 2
-    if (!document.querySelector('.account-add').classList.contains('hidden')) {
-        // for (const iterator of document.querySelectorAll('.account-add span[data-action]')) {
-        //     iterator.classList.toggle('focus');
-        //     index_adding = 0;
-        // }
-
-        if (document.querySelectorAll('.account-add span')[1].classList.contains('focus')) {
-            document.querySelectorAll('.account-add span')[1].classList.remove('focus');
-            document.querySelectorAll('.account-add span')[0].classList.add('focus');
-            index_adding = 0;
+    if (!screenTwo.classList.contains('hidden')) {
+        if (cancelButton.classList.contains('focus')) {
+            cancelButton.classList.remove('focus');
+            addUserButton.classList.add('focus');
+            isActivButton = true;
         }
     }
 
@@ -126,28 +132,21 @@ function leftPress() {
 
 function rightPress() {
     //screen 1
-    if (!document.querySelector('.account-list').classList.contains('hidden')) {
-        for (const iterator of document.querySelectorAll('.item')) {
+    if (!screenOne.classList.contains('hidden')) {
+        for (const iterator of itemList) {
             if (iterator.classList.contains('focus')) {
-                index = iterator.dataset.id;
+                isActivItem = iterator.dataset.id;
             }
             iterator.classList.remove('focus');
         }
-        document.querySelector('span[data-action=newUser]').classList.add('focus');
+        newUserButton.classList.add('focus');
     }
     //screen 2
-    if (!document.querySelector('.account-add').classList.contains('hidden')) {
-
-        // for (const iterator of document.querySelectorAll('.account-add span[data-action]')) {
-        //     iterator.classList.toggle('focus');
-        //     index_adding = -1;
-        // }
-
-
-        if (document.querySelectorAll('.account-add span')[0].classList.contains('focus')) {
-            document.querySelectorAll('.account-add span')[0].classList.remove('focus');
-            document.querySelectorAll('.account-add span')[1].classList.add('focus');
-            index_adding = -1;
+    if (!screenTwo.classList.contains('hidden')) {
+        if (addUserButton.classList.contains('focus')) {
+            addUserButton.classList.remove('focus');
+            cancelButton.classList.add('focus');
+            isActivButton = false;
         }
     }
 
@@ -155,35 +154,29 @@ function rightPress() {
 
 function upPress() {
     //screen 1
-    if (!document.querySelector('.account-list').classList.contains('hidden')) {
-        if (!document.querySelector('.add-block span').classList.contains('focus')) {
-            for (const iterator of document.querySelectorAll('.item')) {
+    if (!screenOne.classList.contains('hidden')) {
+        if (!newUserButton.classList.contains('focus')) {
+            for (const iterator of itemList) {
                 if (iterator.classList.contains('focus') && iterator.previousElementSibling) {
                     iterator.classList.remove('focus');
                     iterator.previousElementSibling.classList.add('focus');
-                    scroll('down');
+                    scroll();
                     return;
                 }
             }
         }
     }
     //screen 2
-    if (!document.querySelector('.account-add').classList.contains('hidden')) {
-
-        // for (const iterator of document.querySelectorAll('.account-add span[data-action]')) {
-        //     iterator.classList.remove('focus');
-        //     index_adding = 0;
-        // }
-        // document.querySelector('.account-add input').focus();
-
-        if (document.querySelectorAll('.account-add span')[0].classList.contains('focus')) {
-            document.querySelectorAll('.account-add span')[0].classList.remove('focus');
-            document.querySelector('.account-add input').focus();
+    if (!screenTwo.classList.contains('hidden')) {
+        if (addUserButton.classList.contains('focus')) {
+            addUserButton.classList.remove('focus');
         }
-        if (document.querySelectorAll('.account-add span')[1].classList.contains('focus')) {
-            document.querySelectorAll('.account-add span')[1].classList.remove('focus');
-            document.querySelector('.account-add input').focus();
+
+        if (cancelButton.classList.contains('focus')) {
+            cancelButton.classList.remove('focus');
         }
+
+        addNewAccountInput.focus();
     }
 
 
@@ -191,38 +184,35 @@ function upPress() {
 
 function downPress() {
     //screen 1
-    if (!document.querySelector('.account-list').classList.contains('hidden')) {
-        if (!document.querySelector('.add-block span').classList.contains('focus')) {
-            for (const iterator of document.querySelectorAll('.item')) {
+    if (!screenOne.classList.contains('hidden')) {
+        if (!newUserButton.classList.contains('focus')) {
+            for (const iterator of itemList) {
                 if (iterator.classList.contains('focus') && iterator.nextElementSibling) {
                     iterator.classList.remove('focus');
                     iterator.nextElementSibling.classList.add('focus');
-                    scroll('up');
+                    scroll();
                     return;
                 }
             }
         }
     }
     //screen 2
-    if (!document.querySelector('.account-add').classList.contains('hidden')) {
-
-      
-        if (index_adding != -1) {
-            document.querySelectorAll('.account-add span')[0].classList.add('focus');
-            document.querySelectorAll('.account-add span')[1].classList.remove('focus');
-            document.querySelector('.account-add input').blur();
-            index_adding = 0;
+    if (!screenTwo.classList.contains('hidden')) {
+        if (isActivButton) {
+            addUserButton.classList.add('focus');
+            cancelButton.classList.remove('focus');
+            isActivButton = true;
         } else {
-            document.querySelectorAll('.account-add span')[1].classList.add('focus');
-            document.querySelectorAll('.account-add span')[0].classList.remove('focus');
-            document.querySelector('.account-add input').blur();
-
+            cancelButton.classList.add('focus');
+            addUserButton.classList.remove('focus');
         }
+
+        addNewAccountInput.blur();
     }
 }
 
 function enterPress() {
-    for (const iterator of document.querySelectorAll('span[data-action]')) {
+    for (const iterator of buttonList) {
         if (iterator.classList.contains('focus')) {
             let actions = iterator.dataset.action;
             if (actions) {
@@ -235,75 +225,57 @@ function enterPress() {
 
 function newUser() {
     clear();
-    document.querySelector('.account-add input').focus();
+    addNewAccountInput.focus();
 }
 
 function addUser() {
-    if (document.querySelector('.account-add input').value) {
+    if (addNewAccountInput.value) {
         let newUser = {
-            "title": document.querySelector('.account-add input').value,
+            "title": addNewAccountInput.value,
             "img": "./images/user.png"
         };
         data.accounts.push(newUser);
         clear();
         LoadUserAccounts(data.accounts);
     } else {
-        alert('Put name');
-        document.querySelector('span[data-action=addUser]').classList.remove('focus');
-        document.querySelector('.account-add input').focus();
+        alert('Enter name');
+        addUserButton.classList.remove('focus');
+        addNewAccountInput.focus();
     }
 }
 
 function cancel() {
     clear();
     LoadUserAccounts(data.accounts);
-    index_adding = 0;
+    isActivButton = true;
 }
 
 function clear() {
-    for (const iterator of document.querySelectorAll('span[data-action]')) {
+    for (const iterator of buttonList) {
         iterator.classList.remove('focus');
     }
-    for (const iterator of document.querySelectorAll('.item')) {
+    for (const iterator of itemList) {
         iterator.classList.remove('focus');
     }
-    for (const iterator of document.querySelectorAll('main>section')) {
+    for (const iterator of screenList) {
         iterator.classList.toggle('hidden');
     }
-    document.querySelector('.account-add input').value = '';
+    addNewAccountInput.value = '';
 }
 
-function scroll(direction) {
-    let itemsTop = document.querySelector('.items').getBoundingClientRect().y;
-    let itemHeight = document.querySelectorAll('.item')[0].getBoundingClientRect().height;
-    let itemTop, item;
-    for (const iterator of document.querySelectorAll('.item')) {
+function scroll() {
+    let itemsTop = items.getBoundingClientRect().y;
+    let itemHeight = itemList[0].getBoundingClientRect().height;
+    let item;
+    for (const iterator of itemList) {
         if (iterator.classList.contains('focus')) {
-            itemTop = iterator.getBoundingClientRect().y;
             item = iterator.offsetTop;
         }
     }
-    switch (direction) {
-        case "up":
-            document.querySelector('.items').scrollTo({
-                top: item - itemsTop - itemHeight,
-                left: 0,
-                behavior: 'smooth'
-            });
-            break;
-        case "down":
-            document.querySelector('.items').scrollTo({
-                top: item - itemsTop - itemHeight,
-                left: 0,
-                behavior: 'smooth'
-            });
-            break;
-        default:
-            document.querySelector('.items').scrollTo({
-                top: item - itemsTop - itemHeight,
-                left: 0,
-                behavior: 'smooth'
-            });
-            break;
-    }
+
+    items.scrollTo({
+        top: item - itemsTop - itemHeight,
+        left: 0,
+        behavior: 'smooth'
+    });
 }
